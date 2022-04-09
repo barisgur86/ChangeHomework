4deddb07a6dfba95d084cf3dc190abb14cdb96df
Baris Gur <barisgur@Bariss-MacBook-Air.local>
2022-04-09 21:53:01 +0300

    Initial commit

---
+++ src/main/java/com/change/ChangeHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.change;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/change/ChangeHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.change;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
